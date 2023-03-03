a37316b1082f3cea0635daf4013c4c304ebf8090
macdonst <simon.macdonald@gmail.com>
2023-03-02 23:23:51 -0500

    Fix handler paths
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ module.exports = {
   set: {
     http: function () {
       return [
-        { method: 'get', path: '/openapi', src: './src/plugins/openapi/handler/all', config: { timeout: 30 } },
-        { method: 'get', path: '/openapi/*', src: './src/plugins/openapi/handler/oneof', config: { timeout: 30 } }
+        { method: 'get', path: '/openapi', src: './node_modules/@enhance/arc-plugin-openapi/src/handler/all', config: { timeout: 30 } },
+        { method: 'get', path: '/openapi/*', src: './node_modules/@enhance/arc-plugin-openapi/src/handler/oneof', config: { timeout: 30 } }
       ]
     },
   },
